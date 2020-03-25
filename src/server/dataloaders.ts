e69eb109e49fd36c5c3ae90bee17275c3cec70fb
Kyle Soeltz <wsoeltz@gmail.com>
2020-03-25 11:58:18 -0400

    Add dataloaders

--- src/server/dataloaders.ts
+++ src/server/dataloaders.ts
@@ -1,14 +1,13 @@
 // Modified from https://sayasuhendra.github.io/graphql-js/7-using-data-loaders/
 // Fixed incorrect IDS with https://github.com/graphql/dataloader/issues/65
-// import DataLoader from 'dataloader';
+import DataLoader from 'dataloader';
+import { Business } from './graphql/schema/queryTypes/businessType';
 
-// export default () => ({
-//   defaultLoader: new DataLoader(
-//     async keys => Promise.all(
-//       keys.map(key => State.findOne({_id: key})),
-//     ),
-//     {cacheKeyFn: (key: any) => key.toString()},
-//   ),
-// });
-
-export default () => {return};
+export default () => ({
+  businessLoader: new DataLoader(
+    async keys => Promise.all(
+      keys.map(key => Business.findOne({_id: key})),
+    ),
+    {cacheKeyFn: (key: any) => key.toString()},
+  ),
+});
